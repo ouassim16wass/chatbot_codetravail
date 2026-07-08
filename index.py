57d6e66cfa583206d58af96115ace9f3e8af1524
ouassim16wass <wassimmegrad1@gmail.com>
2026-07-08 17:16:24 +0200

    Nettoyage du code

--- index.py
+++ index.py
@@ -1,10 +1,4 @@
-"""Script d'indexation : construit la base vectorielle depuis le corpus
-et la sauvegarde sur disque."""
-
-
 def main():
-    # TODO: charger le corpus (data/), decouper en chunks,
-    # encoder avec le modele d'embedding et persister la base
     pass
 
 
